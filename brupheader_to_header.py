92b8cd02bb1edb5f03b184efb0414ff174419e78
111 <11875409@qq.com>
2019-08-01 11:11:59 +0800

    修改json换行

--- brupheader_to_header.py
+++ brupheader_to_header.py
@@ -34,19 +34,21 @@ def zhuanhuan(header):
                 result_l = '\'' + ge[0] + '\''
                 result_m = '\'' + ge[1] + '\''
             gai = result_l + ':' + result_m
-            result = result  + gai + "\n"
+            result = result  + gai + ",\n"
     return result
 
 
 if __name__ == '__main__':
     resulr = zhuanhuan('''
-        User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0
-    Accept: application/json, text/javascript, */*; q=0.01
-    Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
-    Accept-Encoding: gzip, deflate
-    Referer: http://ld.ldfaka.com/links/ECAC0DADFF37B100
-    Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-    X-Requested-With: XMLHttpRequest
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0
+Accept: application/json, text/javascript, */*; q=0.01
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Accept-Encoding: gzip, deflate
+Referer: http://1111
+Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+X-Requested-With: XMLHttpRequest
+Content-Length: 11
+Connection: close
     
     ''')
     print(resulr)
