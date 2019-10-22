f85c30db20b3fabd9f46d49b398060a6afaa047c
thatqier <thatqier@gmail.com>
2019-10-22 19:01:31 +0800

    修改头文件错误

--- brupheader_to_header.py
+++ brupheader_to_header.py
@@ -3,7 +3,7 @@
 
 
 """
-@version: 0.1
+@version: 0.2
 @author: thatqier
 @contact: 11875409@qq.com
 @site: 1
@@ -14,15 +14,6 @@
 """
 
 def zhuanhuan(header):
-    test = '''
-    User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0
-    Accept: application/json, text/javascript, */*; q=0.01
-    Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
-    Accept-Encoding: gzip, deflate
-    Referer: wwww
-    Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-    X-Requested-With: XMLHttpRequest
-    '''
     header = header.split('\n')
     result = ""
     for i in range(0,len(header)):
@@ -30,25 +21,16 @@ def zhuanhuan(header):
         if(":" in chang):
             chang = chang.replace(' ','')
             ge = chang.split(':')
-            for m in range(0,1):
-                result_l = '\'' + ge[0] + '\''
-                result_m = '\'' + ge[1] + '\''
+            result_l = '\'' + ge[0] + '\''
+            result_m = '\''
+            for m in range(1,len(ge)):
+                result_m = result_m +str(ge[m])+':'
+            result_m = result_m[:-1]  + '\''
             gai = result_l + ':' + result_m
             result = result  + gai + ",\n"
     return result
 
 
 if __name__ == '__main__':
-    resulr = zhuanhuan('''
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0
-Accept: application/json, text/javascript, */*; q=0.01
-Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
-Accept-Encoding: gzip, deflate
-Referer: http://1111
-Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-X-Requested-With: XMLHttpRequest
-Content-Length: 11
-Connection: close
-    
-    ''')
+    resulr = zhuanhuan('''''')
     print(resulr)
